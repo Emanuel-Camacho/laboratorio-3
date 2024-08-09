37be0958009315a1c79dd71ab8145f1305efc77d
TommyGun1510 <bytomas135@gmail.com>
2024-08-09 15:35:46 -0300

    Update practice1.js

--- practice/practice1.js
+++ practice/practice1.js
@@ -131,7 +131,9 @@ let watchList = [
 ];
 
 const returnMovieTitle = (watchlist) => {
-    return watchlist.filter(movie => movie.Director === 'Christopher Nolan' && parseFloat(movie.imdbRating) > 8.0).map(movie => movie.Title);
+    return watchlist
+        .filter(movie => movie.Director === 'Christopher Nolan' && parseFloat(movie.imdbRating) > 8.0)
+        .map(movie => movie.Title);
 }
 
 const titlesArray = returnMovieTitle(watchList);
